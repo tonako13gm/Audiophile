9d27d31bf3e61e4c993997d6a3e5ff6250a406e4
Anthony Sylvester Bahinting <tonako13gm@gmail.com>
2025-10-20 11:17:33 +0800

    revert back to enabling cart to check again for testing

--- src/app/@cartModal/(.)cart/page.tsx
+++ src/app/@cartModal/(.)cart/page.tsx
@@ -2,8 +2,6 @@
 import CartCheckOut from '@/src/app/components/cart/checkOutCart';
 import Modal from "@/src/app/components/modal";
 
-// import { Modal } from './ssr';
-
 export default function ModalCart() {
   const style = 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:w-10/12 md:w-8/12 lg:w-2/5 max-h-[90vh] overflow-hidden bg-lightColor rounded-lg shadow-xl overflow-y-auto'
 
